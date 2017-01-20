027b118d5f9104109e188bb5fd130213cef10cc7
Steve Lu <splu10@yahoo.com>
2017-01-20 14:04:54 -0500

    Add annotation '@JsonDeserialize'

--- usvcs/actor-api/src/main/java/com/catassetintel/mai/usvcs/actors/api/EchoReply.java
+++ usvcs/actor-api/src/main/java/com/catassetintel/mai/usvcs/actors/api/EchoReply.java
@@ -2,7 +2,10 @@ package com.catassetintel.mai.usvcs.actors.api;
 
 import org.immutables.value.Value;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 @Value.Immutable
+@JsonDeserialize
 public abstract class EchoReply {
 	public abstract String said();
 }
